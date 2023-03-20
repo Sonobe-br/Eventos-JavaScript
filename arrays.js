b5bd8e0f7255743e59a785e34c156b01e18317d4
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2023-03-20 13:40:00 -0300

    Commitando arrays01

---
+++ arrays.js
@@ -0,0 +1,42 @@
+//ARRAY LITERAL
+/* const listaEngrenagem = [
+    "Engrenagem motor - 01",
+    "Engrenagem motor - 02",
+    "Engrenagem motor - 03",
+    "Engrenagem motor - 04"
+];
+const listaCorreia = [
+    "Correia motor - 01",
+    "Correia motor - 02",
+    "Correia motor - 03",
+    "Correia motor - 04"
+];
+const listaCambio = [
+    "Cambio carro - 01",
+    "Cambio carro - 02",
+    "Cambio carro - 03",
+    "Cambio carro - 04"
+];
+const listaEmbreagem = [
+    "Embreagem carro - 01",
+    "Embreagem carro - 02",
+    "Embreagem carro - 03",
+    "Embreagem carro - 04"
+];
+const allList = listaEngrenagem.concat(listaCorreia, listaCambio);
+document.getElementById("teste").innerHTML = allList;
+console.log(listaEngrenagem); */
+
+/* let X = lista[3];
+alert(X);
+lista.push ("Engrenagem motor - 05")
+*/
+//alert (listaEngrenagem[listaEngrenagem.length -1]);
+//alert(Array.isArray(lista01)); //identifica se a variável é uma matriz
+//document.getElementById("teste").innerHTML = listaEngrenagem.join( " - " )
+
+/* 
+método pop remove o último item da matriz
+método shift remove o primeiro item da matriz
+método unshift inseri um item na primeira posição da matriz
+*/
---
+++ slice.js
@@ -0,0 +1,60 @@
+//ARRAY - MÉTODO SLICE - MATRIZ FATIADA 
+//ARRAY - MÉTODO SORT - MATRIZ ORDENADA EM ORDEM ALFABÉTICA PARA STRINGS
+//ARRAY - MÉTODO REVERCE - MATRIZ ORDENADA DE TRÁS PARA FRENTE 
+/* const listaDeProdutos = [
+    "Engrenagem motor  00",
+    "Engrenagem motor  01",
+    "Engrenagem motor  02",
+    "Engrenagem motor  03",
+    "Correia motor     04",
+    "Correia motor     04",
+    "Correia motor     06",
+    "Correia motor     07",
+    "Cambio carro      08",
+    "Cambio carro      09",
+    "Cambio carro      10",
+    "Cambio carro      11",
+    "Disco de Freio    12",
+    "Disco de Freio    13",
+    "Disco de Freio    14",
+    "Disco de Freio    15"
+];
+const correiasDentadas = listaDeProdutos.slice(4, 8);
+const odernandoMatriz = listaDeProdutos.sort();
+odernandoMatriz.sort();
+odernandoMatriz.reverse();
+document.getElementById("texto").innerHTML = odernandoMatriz;
+ */
+
+//FUNÇÃO PARA ORDENAÇÃO NÚMERICA
+/* const listagemPorCodigos = [
+    10002,
+    90022,
+    20043,
+    70452,
+    51090,
+    29002,
+    10972
+];
+
+listagemPorCodigos.sort(function(a, b) { 
+    return a - b
+});  
+document.getElementById("texto").innerHTML = listagemPorCodigos; */
+
+//FUNÇÃO PARA CAPTURAR O MAIOR NÚMERO
+const listagemPorCodigos = [
+    10002,
+    90022,
+    20043,
+    70452,
+    51090,
+    29002,
+    10972
+];
+
+function capturandoMaiorNumero (array) {
+    return Math.max.apply(null,array);
+}
+
+document.getElementById("texto").innerHTML = capturandoMaiorNumero(listagemPorCodigos);
