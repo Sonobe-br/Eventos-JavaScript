928d84eec9df1f34f2df214142cf5e2a050d54bc
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2023-04-02 17:41:19 -0300

    ativando a função parando contagem

--- contagem.js
+++ contagem.js
@@ -2,9 +2,15 @@ function ativarContagem() {
     document.getElementById("tempo").innerHTML = " Começou a contagem";
     
     //ativa a função somente uma vez baseado no tempo determinado
-    setTimeout(function() { 
-        document.getElementById("tempo").innerHTML = " Executou o setTimeOut";
+    tempo = setTimeout(function() { 
+        document.getElementById("tempo").innerHTML = "Executou o setTimeOut";
         document.body.style.backgroundColor = "orange";
-     }, 5000);
+    }, 5000);
+    
+}
+
+function pararContagem() {
+    clearTimeout(tempo);
+    document.getElementById("tempo").innerHTML = "Parou a contagem";
 
 }
