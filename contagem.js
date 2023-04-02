a45984f1f13e57f13a385d9b6856418a717a3262
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2023-04-02 17:48:40 -0300

    setInterval

--- contagem.js
+++ contagem.js
@@ -1,16 +1,25 @@
 function ativarContagem() {
-    document.getElementById("tempo").innerHTML = " Começou a contagem";
-    
+    //document.getElementById("tempo").innerHTML = " Começou a contagem"; 
+
     //ativa a função somente uma vez baseado no tempo determinado
-    tempo = setTimeout(function() { 
+    /* tempo = setTimeout(function() { 
         document.getElementById("tempo").innerHTML = "Executou o setTimeOut";
         document.body.style.backgroundColor = "orange";
     }, 5000);
+    */
+   
+    setInterval(function() {
     
+        let cronometro = document.getElementById("tempo").innerHTML;
+       let soma = parseInt(cronometro) + 1;
+       document.getElementById("tempo").innerHTML = soma;
+    
+    }, 1000);
 }
 
-function pararContagem() {
-    clearTimeout(tempo);
-    document.getElementById("tempo").innerHTML = "Parou a contagem";
 
+function pararContagem() {
+    /* clearTimeout(tempo);
+    document.getElementById("tempo").innerHTML = "Parou a contagem";
+ */
 }
