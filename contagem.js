cfd2e0ab6aa0c74b22e2e93d755ef09d2bf42723
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2023-04-02 17:37:47 -0300

    adicionando comentário

--- contagem.js
+++ contagem.js
@@ -1,5 +1,7 @@
 function ativarContagem() {
     document.getElementById("tempo").innerHTML = " Começou a contagem";
+    
+    //ativa a função somente uma vez baseado no tempo determinado
     setTimeout(function() { 
         document.getElementById("tempo").innerHTML = " Executou o setTimeOut";
         document.body.style.backgroundColor = "orange";
